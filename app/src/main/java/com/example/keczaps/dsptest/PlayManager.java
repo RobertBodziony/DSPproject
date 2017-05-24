ec305f2777065077f7e5cef40e7cf5a58d6d98d0
Keczaps <robertbodziony93@gmail.com>
2017-05-25 01:58:33 +0200

    ver1.1 - UI Improved. // TODO : Lower SamplingRate / NEW ACTIVITY

--- app/src/main/java/com/example/keczaps/dsptest/PlayManager.java
+++ app/src/main/java/com/example/keczaps/dsptest/PlayManager.java
@@ -9,6 +9,12 @@ import android.util.Log;
 
 import com.musicg.wave.Wave;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -20,7 +26,8 @@ public class PlayManager{
     private int threadPoolNumberForTask;
     private double duration = 0.1;
     private double numSample = duration*sampleRate;
-    private double sample[] = new double[(int) numSample];
+    private double numSample2 = 2*numSample;
+    private double sample[] = new double[(int) numSample/2];
     private byte[] generatedSnd = new byte[(int) numSample];
     private byte[] finalGeneratedSignal;
     private Handler playHandler = new Handler();
@@ -30,7 +37,26 @@ public class PlayManager{
     public PlayManager(int threadPoolNumberForTask) {
         this.threadPoolNumberForTask = threadPoolNumberForTask;
         finalGeneratedSignal = getWaveFileBytes();
-        //this.genTone(5000,6000);
+        this.genTone(2000,4000,"signal25ms441k2to3k.wav");
+        this.genTone(3000,5000,"signal25ms441k3to4k.wav");
+        this.genTone(4000,6000,"signal25ms441k4to5k.wav");
+        this.genTone(5000,7000,"signal25ms441k5to6k.wav");
+        this.genTone(2500,4500,"signal25ms441k25to35k.wav");
+        this.genTone(3500,5500,"signal25ms441k35to45k.wav");
+        this.genTone(4500,6500,"signal25ms441k45to55k.wav");
+        this.genTone(5500,7500,"signal25ms441k55to65k.wav");
+        this.sampleRate = 22050;
+        this.numSample = duration*sampleRate;
+        this.sample = new double[(int) (numSample/2)];
+        this.generatedSnd = new byte[(int) (numSample*2)];
+        this.genTone(2000,4000,"signal25ms221k2to3k.wav");
+        this.genTone(3000,5000,"signal25ms221k3to4k.wav");
+        this.genTone(4000,6000,"signal25ms221k4to5k.wav");
+        this.genTone(5000,7000,"signal25ms221k5to6k.wav");
+        this.genTone(2500,4500,"signal25ms221k25to35k.wav");
+        this.genTone(3500,5500,"signal25ms221k35to45k.wav");
+        this.genTone(4500,6500,"signal25ms221k45to55k.wav");
+        this.genTone(5500,7500,"signal25ms221k55to65k.wav");
     }
 
     public void startPlaying() {
@@ -72,11 +98,11 @@ public class PlayManager{
     }
 
 
-    public void genTone(double freqDOWN,double freqUP){
+    public void genTone(double freqDOWN,double freqUP,String fnm){
 
         double instfreq=0,numerator;
 
-        for (int i=0;i<numSample; i++ ) {
+        for (int i=0;i<(numSample-1)/2; i++ ) {
             numerator = (double)(i)/numSample;
             instfreq = freqDOWN+(numerator*(freqUP-freqDOWN));
             if ((i % 1000) == 0) {
@@ -91,6 +117,53 @@ public class PlayManager{
             final short val = (short) ((dVal * 32767));
             generatedSnd[idx++] = (byte) (val & 0x00ff);
             generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+        }
+
+        writeAudioDataToFile(fnm,sampleRate);
+    }
+
+    private void writeAudioDataToFile(String fnm,int smplRate) {
+        WaveFormatManager waveFormatManager = new WaveFormatManager();
+        String fileName = fnm;//new SimpleDateFormat("yyyyMMddhhmm'.wav'").format(new Date());
+        String filePath=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getPath()+"/DSPtestFiles/";
+        String fname=filePath+fileName;
+
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(fname);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.e("FileNotFoundException", e.toString());
+        }
+
+        try {
+            if(os != null){
+                waveFormatManager.writeWavHeader(os, AudioFormat.CHANNEL_IN_MONO, smplRate, AudioFormat.ENCODING_PCM_16BIT);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+            try {
+                os.write(generatedSnd, 0, generatedSnd.length/2);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("os = not write to file", e.toString());
+            }
+
+        try {
+            if(os != null){
+                os.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("os = null close", e.toString());
+        }
+
+        try {
+            waveFormatManager.updateWavHeader(new File(fname));
+        } catch (IOException ex) {
+            ex.getMessage();
         }
     }
 
---
+++ app/src/main/java/com/example/keczaps/dsptest/StartupActivity.java
@@ -0,0 +1,29 @@
+package com.example.keczaps.dsptest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Keczaps on 2017-05-25.
+ */
+
+public class StartupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
